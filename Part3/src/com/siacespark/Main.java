1f89b60b31873ddef6efcfab9037cbc2b44e6358
SiAce <siacespark@gmail.com>
2021-06-12 13:03:43 -0700

    start part 3

---
+++ Part3/src/com/siacespark/Main.java
@@ -0,0 +1,13 @@
+package com.siacespark;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s = null;
+        System.out.println(s.equals(null));
+    }
+}
