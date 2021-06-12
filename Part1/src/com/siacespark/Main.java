914ab62dad5dad69f85c01a68639cfe8c54ed20c
SiAce <siacespark@gmail.com>
2021-06-11 21:19:00 -0700

    system output and input

---
+++ Part1/src/com/siacespark/Main.java
@@ -0,0 +1,20 @@
+package com.siacespark;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Print "Write a message: "
+        System.out.println("Write a message: ");
+
+        // Read the string written by the user, and assign it
+        // to program memory "String message = (string that was given as input)"
+        String message = scanner.nextLine();
+
+        // Print the message written by the user
+        System.out.println(message);
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Learn Java (Again for the Job)
+
+Code base for the course [Java Programming](https://java-programming.mooc.fi) on [MOOC.fi](https://www.mooc.fi).
+
